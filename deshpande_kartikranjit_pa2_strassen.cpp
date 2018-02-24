3819c61d05d48e923446a4b0e01c9c4f9f9caed5
Kartik Ranjit Deshpande <kartikdeshpande18@gmail.com>
2018-02-23 22:50:48 -0500

    Added function to perform normal multiplication

--- deshpande_kartikranjit_pa2_strassen.cpp
+++ deshpande_kartikranjit_pa2_strassen.cpp
@@ -7,8 +7,8 @@ using namespace std;
 class MatrixMultiply
 {
 	public:
-		int first_matrix[100][100], second_matrix[100][100];
-
+		int first_matrix[111][111], second_matrix[111][111], result_matrix_normal[111][111], result_matrix_strassen[111][111];
+		const int MAX_ARR_SIZE = 111;
 		/*
 		** Returns a randomly generated number
 		*/
@@ -34,10 +34,63 @@ class MatrixMultiply
 					first_matrix[i][j] = generate_random_no();
 				}
 			}
-				
-		}
-	
 
+			// Second matrix
+                        for (int i = 0; i < 4; i++ )
+                        {
+                                for ( int j = 0; j < 4; j++ )
+                                {
+                                        second_matrix[i][j] = generate_random_no();
+                                }
+                        }
+			//display_matrix();
+		}
+
+		/*
+		** Display a matrix
+		*/	
+		void display_matrix(int arg_matrix[111][111])
+		{
+			for (int i = 0; i < 4; i++ )
+                        {
+                                for ( int j = 0; j < 4; j++ )
+                                {
+                              		cout << arg_matrix[i][j] << " ";
+                                }
+				cout << "\n";
+                	}
+			cout << "\n";
+		}
+
+		/*
+		** Function to perform normal multiplication
+		*/
+		void perform_normal_multiplication()
+		{
+			// initialize all elements of result matrix to 0 
+			for (int i = 0; i < 4; i++ )
+                        {
+                                for ( int j = 0; j < 4; j++ )
+                                {
+                                        result_matrix_normal[i][j] = 0 ;
+                                }
+                        }
+
+			// Multiply both matrices
+			for (int i = 0; i < 4; i++ )
+                        {
+                                for ( int j = 0; j < 4; j++ )
+                                {
+					for ( int k = 0; k < 4; k ++ )
+					{
+                                        	result_matrix_normal[i][j] = result_matrix_normal[i][j] + first_matrix[i][j] * second_matrix[i][j] ;
+					}
+                                }
+                        }
+			display_matrix(first_matrix);
+			display_matrix(second_matrix);
+			display_matrix(result_matrix_normal);
+		}
 };
 
 int validate_input_data_n(int n)
@@ -61,5 +114,6 @@ int main(int argc, char * argv[])
 	int input_data_n ;
 	MatrixMultiply m1; 
 	m1.input_matrix();	
+	m1.perform_normal_multiplication();
 }
 
