fc3796636eac1bfee6d765f65a6540a2682660ea
Kartik Ranjit Deshpande <kartikdeshpande18@gmail.com>
2018-02-24 22:57:31 -0500

    Basic struct

--- deshpande_kartikranjit_pa2_lim.cpp
+++ deshpande_kartikranjit_pa2_lim.cpp
@@ -5,18 +5,30 @@ using namespace std;
 
 class LargeIntMul
 {
-      
+	public:
+		string number1 = "12345" , number2 = "123123";
+	      
+		string prod(string numb1, string numb2)	
+		{
+			// No of digits in numb1 and numb2
+			int n = numb1.size();
+			int m = numb2.size();
+
+			if ( n == 0 || m == 0 )
+			{
+				// anything multiplied by 0 is 0
+				return "0";
+			}	
+
+			return numb1;
+		}
 	
-}
+};
 
 
 int main(int argc, char * argv[])
 {
-        std::string userinput;
-        int input_data_n ;
-        MatrixMultiply m1(4);
-        m1.input_matrix();
-        m1.perform_normal_multiplication();
-        m1.perform_strassens_multiplication();
+       	LargeIntMul m1;
+	m1.prod("1231","12123123");
 }
 
