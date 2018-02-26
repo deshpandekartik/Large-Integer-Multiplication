cb4ca275a84e570c95c77b30059a6e8a0f44dea3
Kartik Ranjit Deshpande <kartikdeshpande18@gmail.com>
2018-02-25 19:38:25 -0500

    Substracting two big int numbers done

--- deshpande_kartikranjit_pa2_lim.cpp
+++ deshpande_kartikranjit_pa2_lim.cpp
@@ -51,19 +51,87 @@ class LargeIntMul
 					{
 						return 1;
 					}
-					else if ( a[i] < b[i] ) {
+					else if ( a[i] < b[i] ) 
+					{
 						return -1;
 					}
 				}
                         }
                 }
 
-		string division(string number, string divisor)
+		/*
+		** Substracting two large numnbers in string format
+		*/
+		string sub(string number1, string number2)
 		{
+			while( number1.length() < number2.length() )
+			{
+				number1.insert(0,"0");
+			}
+
+			while( number2.length() < number1.length() )
+                        {
+                                number2.insert(0,"0");
+                        }
+
+			cout << number1 << " " << number2 << endl;	
+			string result, sub_res ;
+			int carry = 0;
+			for ( int i = number1.size() - 1; i >=0 ; i-- )
+			{
+				int num1 = number1.at(i);
+				int num2 = number2.at(i) + carry;
+				if ( num1 == num2 )
+				{
+					result.insert(0,"0");	
+					carry = 0;
+				}
+				else if ( num1 > num2 )
+				{
+					result.insert(0,to_string(num1 - num2));
+					carry = 0;
+				}
+				else if ( num1 < num2 )
+				{
+					string cary_res =  to_string((num1 + 10) - num2);
+					result.insert(0,cary_res.substr(cary_res.size() - 1 , cary_res.size()));
+					cary_res = cary_res.substr(0,cary_res.size() - 1);
+					carry = atoi(cary_res.c_str()) ;
+					carry = carry + 1;
+					 cout << carry << "asda \n";
+				}
+			}
+			cout << result;
+			return "";
+		}
+
+		/*
+		** Divsion of two large numbers in string
+		*/
+		string div(string number, string divisor)
+		{
+			// if in case both number and divisor are equal
+                        if ( number == divisor )
+                        {
+                                return "1";
+                        }
+
+                        // if divisor > number
+                        if ( divisor.size() > number.size() )
+                        {
+                                return "0";
+                        }
+
+			string quotient ;
+			/*
 			while ( compare(number , divisor) == 1 )
 			{
-	
+				//number = number - divisor
+				// quotient ++;	
 			}
+			*/
+			cout << sub(number, divisor);
+			return "";
 		}
 
 
@@ -72,52 +140,6 @@ class LargeIntMul
 		*/
 		string modulus(string number, string divisor )
 		{
-			// if in case both number and divisor are equal
-			if ( number == divisor )
-			{
-				return "1";
-			}
-
-			// if divisor > number
-			if ( divisor.size() > number.size() )
-			{
-				return "0";
-			}
-
-			int number_end_index = divisor.size();
-			bool flag = true;
-			string remainder = "";
-			string quotient;
-
-			while ( compare(number , divisor) == 1 )  
-			{
-				// if number is gt then or equal to divisor proceed with divison else add one more digit
-				if ( compare(number.substr(0,number_end_index) , divisor) != 1 )
-				{
-					number_end_index = number_end_index + 1;
-					// divisor does not go into quotient , append a 0 to quotient
-					quotient.append("0");
-				}
-				remainder = to_string(atoi(number.substr(0,number_end_index).c_str()) % atoi(divisor.c_str()));
-				quotient.append(to_string(atoi(number.substr(0,number_end_index).c_str()) / atoi(divisor.c_str())));
-				number.erase(0,number_end_index);
-				number.insert(0,remainder);
-			}
-
-			// remove all first '0' occurances from quotient
-			for(char& c : quotient) 
-			{
-				if ( c == '0' )
-				{
-					quotient.erase(0,1);
-				}
-				else
-				{
-					break;
-				}
-			} 	
-			
-			return remainder;
 		}		
 
 		/*
@@ -142,7 +164,7 @@ class LargeIntMul
 			}
 
 			int m = floor(n/2); 	//	m = [ n/2 ]
-			modulus(numb1,numb2);
+			div(numb1,numb2);
 
 			return numb1;
 		}
@@ -153,6 +175,6 @@ class LargeIntMul
 int main(int argc, char * argv[])
 {
        	LargeIntMul m1;
-	cout << m1.modulus("756812","12") << "\n";
+	cout << m1.div("213234114","1231234") << "\n";
 }
 
